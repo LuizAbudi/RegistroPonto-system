66e59e4cf5d55a48a34f9a3658e18e9542607032
LuizAbudi <luizabudi@hotmail.com>
2023-09-19 16:16:12 -0300

    adding buttao showPassword

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -3,11 +3,14 @@ import "./LoginPage.css";
 import imagem from "./img/Logo-preto.png";
 import { authenticateUser } from "../../services/authServices";
 import { useNavigate } from "react-router-dom";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,24 +32,34 @@ const LoginPage = () => {
       <div className="logo">
         <img src={imagem} alt="Logo" className="logo-image" />
       </div>
-      <form
-      //  onChange={handleSubmit}
-      >
+      <form>
         <div className="login-box">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Digite seu Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password" // Atualizado o tipo de input para "password".
-            className="form-control"
-            placeholder="Digite sua Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder=" Digite seu Usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="password-container">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control"
+              placeholder=" Digite sua Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <span
+              className="toggle-password-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </span>
+          </div>
+
           <button type="submit" className="button" onClick={handleSubmit}>
             Entrar
           </button>
