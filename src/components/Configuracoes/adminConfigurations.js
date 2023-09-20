106a66932be48fafddd67413b48dccfc6a3c8eb3
LuizAbudi <luizabudi@hotmail.com>
2023-09-20 09:42:35 -0300

    configurations

---
+++ src/components/Configuracoes/adminConfigurations.js
@@ -0,0 +1,11 @@
+import "./adminConfigurations.css";
+import React from 'react'
+
+function adminConfigurations() {
+
+  return (
+    <div>adminConfigurations</div>
+  )
+}
+
+export default adminConfigurations;
