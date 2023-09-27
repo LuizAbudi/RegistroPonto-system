a9d8e813cc7db47f273f42c0d0d99df6e0926dfd
LuizAbudi <luizabudi@hotmail.com>
2023-09-27 11:44:33 -0300

    fake ponto generator

--- src/tests/PontoTester.js
+++ src/tests/PontoTester.js
@@ -1,28 +1,59 @@
-const setPontos = (day, month, year) => {
-  const dateKey = `${day}/${month}/${year}`;
-  const dataValue = [
-    { data: dateKey, hora: "17:05:26" },
-    { data: dateKey, hora: "00:20:49" },
+export default function PontoTester() {
+  const setPonto = (funcionario, day, month, year) => {
+    const entrada = `${randomHour(8, 10)}:${randomMinute()}:${randomSecond()}`;
+    const saida = `${randomHour(17, 19)}:${randomMinute()}:${randomSecond()}`;
+    const funcionarios = `${funcionario}`;
+    const data = `${day}/${month}/${year}`;
+
+    const dataValue = {
+      funcionario: funcionarios,
+      data: data,
+      entrada: entrada,
+      saida: saida,
+    };
+
+    // Recupere os registros existentes do funcionário
+    const existingData = JSON.parse(
+      localStorage.getItem("Funcionário") || "[]"
+    );
+    existingData.push(dataValue); // Adicione o novo registro à lista
+
+    localStorage.setItem("Funcionário", JSON.stringify(existingData));
+  };
+
+  const randomHour = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const randomMinute = () => {
+    return String(Math.floor(Math.random() * 60)).padStart(2, "0");
+  };
+
+  const randomSecond = randomMinute;
+
+  let day = 23;
+  let month = 9;
+  let year = 2023;
+  const funcionarios = [
+    "Funcionario1",
+    "Funcionario2",
+    "Funcionario3",
+    "Funcionario4",
   ];
 
-  localStorage.setItem(dateKey, JSON.stringify(dataValue));
-};
+  for (let i = 0; i < 30; i++) {
+    funcionarios.forEach((funcionario) => {
+      setPonto(funcionario, day, month, year);
+    });
 
-let day = 23;
-let month = 9;
-let year = 2023;
-
-for (let i = 0; i < 20; i++) {
-  setPontos(day, month, year);
-
-  day++;
-  if (day > 30 && month === 9) {
-    //considerando setembro com 30 dias
-    day = 1;
-    month++;
-  }
-  if (month > 12) {
-    month = 1;
-    year++;
+    day++;
+    if (day > 30 && month === 9) {
+      day = 1;
+      month++;
+    }
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
   }
 }
